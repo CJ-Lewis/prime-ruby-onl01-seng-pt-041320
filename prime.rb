92c2a13c18f004b537da0e619c619dd2d8d39566
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-26 12:26:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(number)
   start = 2
-  if 
-    (2..value - 1).each do |i|
+  if number > 1
+    (2..value- 1).each do |i|
       if value % i = 0
         return false
       end
